fb584bce62558f043c3fe8c50eefdcf70e038c16
nacho <ignaciobarco@hotmail.com>
2020-01-29 23:55:57 +0100

    cambiamos el nombre a wallaApi

--- wallaAPI/app.js
+++ wallaAPI/app.js
@@ -1,29 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-app.engine('html', require('ejs').__express);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
+app.engine("html", require("ejs").__express);
 
 // Middlewares
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //app.use('/pdf', express.static(path.join(__dirname, '/mnt/sda3/images/pdfs')));
 
 /**
  * Conexión con la base de datos
  */
-require('./lib/connectMongoose');
-require('./models/Agente');
+require("./lib/connectMongoose");
+require("./models/Agente");
 
 app.use((req, res, next) => {
   // Una de 2 cosas:
@@ -40,15 +40,15 @@ app.use((req, res, next) => {
 /**
  * Rutas de mi API
  */
-app.use('/apiv1/agentes', require('./routes/apiv1/agentes'));
+app.use("/apiv1/agentes", require("./routes/apiv1/agentes"));
 
-app.locals.title = 'NodeAPI';
+app.locals.title = "wallaApi";
 
 /**
  * Rutas de mi aplicación web
  */
-app.use('/',        require('./routes/index'));
-app.use('/users',   require('./routes/users'));
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,12 +58,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // comprobar error de validación
-  if (err.array) { // error de validación
+  if (err.array) {
+    // error de validación
     err.status = 422;
     const errInfo = err.array({ onlyFirstError: true })[0];
-    err.message = isAPI(req) ?
-      { message: 'Nor valid', errors: err.mapped()} :
-      `Not valid - ${errInfo.param} ${errInfo.msg}`;
+    err.message = isAPI(req)
+      ? { message: "Nor valid", errors: err.mapped() }
+      : `Not valid - ${errInfo.param} ${errInfo.msg}`;
   }
 
   res.status(err.status || 500);
@@ -75,14 +76,14 @@ app.use(function(err, req, res, next) {
 
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.render('error');
+  res.render("error");
 });
 
 function isAPI(req) {
-  return req.originalUrl.indexOf('/apiv') === 0;
+  return req.originalUrl.indexOf("/apiv") === 0;
 }
 
 module.exports = app;
