711fef150dd3bd64848e9e66e3614935864b205f
Nacho <ignaciobarco@hotmail.com>
2020-02-21 19:41:55 +0100

    mostramos los campos necesareos en la lista de ads

--- wallaReact/src/components/BuildAdvertsList.jsx
+++ wallaReact/src/components/BuildAdvertsList.jsx
@@ -11,44 +11,32 @@ const buildAdvertsList = ({
   photo,
   description,
   sell,
-  price,
-  author,
-  date,
-  tags,
-  reserved,
-  sold,
-  chat
+  price
+  // author,
+  // date,
+  // tags,
+  // reserved,
+  // sold,
+  // chat
 }) => (
-    <div key={_id}>
-      <Card style={{ width: "20rem" }}>
-        <Card.Img variant="top" src={`http://localhost:8080${photo}`} />
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>
-            Descripción: {description}
-            <br />
-            {sell}
-            Precio: {price}€
-            <br />
-            Autor: {author}
-            <br />
-            Fecha: {date}
-            <br />
-            tags: {tags}
-            <br />
-            reservado: {reserved.toString()}
-            <br />
-            Vendido: {sold.toString()}
-            <br />
-            Chat: {chat}
-          </Card.Text>
-          <Link to={`/advert/${_id}`}>
-            <Button variant="primary">Detalles</Button>
-          </Link>
-        </Card.Body>
-      </Card>
-    </div>
-  );
-
+  <div key={_id}>
+    <Card style={{ width: "20rem" }}>
+      <Card.Img variant="top" src={`http://localhost:8080${photo}`} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          Descripción: {description}
+          <br />
+          {(sell && <div>Se vende</div>) || <div>Se busca</div>}
+          Precio: {price}€
+          <br />
+        </Card.Text>
+        <Link to={`/advert/${_id}`}>
+          <Button variant="primary">Detalles</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  </div>
+);
 
 export default buildAdvertsList;
